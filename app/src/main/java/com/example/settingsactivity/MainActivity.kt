a39c85375bc76790917aa9b006504e9434148268
antialopez <antialopezgarcia@gmail.com>
2023-04-20 18:43:33 +0200

    dataStore and changeListener rangeSlider

--- app/src/main/java/com/example/settingsactivity/MainActivity.kt
+++ app/src/main/java/com/example/settingsactivity/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.example.settingsactivity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.settingsactivity.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+//Delegado. Crear una unica instancia de la base de datos
+//Singleton, para tener una unica instania en la bas de datos
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val  VOLUME_LVL = "volume_lvl"
+    }
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
     }
+
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener{_,value,_ ->
+            Log.i("antia", "EL valor es $value")
+
+        }
+    }
+
 }
